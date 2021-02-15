e897cbc9acab25c8b35e11c676d219b4f40e9c0e
EhsanTgv <EhsanTgv@gmail.com>
2021-02-15 21:35:56 +0330

    show DrawerMenu in Home

--- lib/home.dart
+++ lib/home.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_lorem/flutter_lorem.dart';
+import 'package:provider/provider.dart';
+
+import 'drawer_menu.dart';
+import 'model/ui.dart';
 
 class Home extends StatelessWidget {
+  String text = lorem(paragraphs: 3, words: 50,);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
+      ),
+      drawer: DrawerMenu(),
+      body: Container(
+        margin: EdgeInsets.all(10),
+        child: Consumer<UI>(
+          builder: (context, ui, child) {
+            return RichText(
+              text: TextSpan(
+                text: text,
+                style: TextStyle(fontSize: ui.fontSize, color: Colors.black),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
